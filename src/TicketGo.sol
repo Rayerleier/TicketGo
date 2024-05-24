dfd500d7e1622e9c9a7f6335a360b9073e82d0e1
Carry Wang <design.carrywang@gmail.com>
2024-05-24 17:22:23 +0800

    Add chainlink ref

--- src/TicketGo.sol
+++ src/TicketGo.sol
@@ -1,14 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract TicketGo {
-    uint256 public number;
+import {AggregatorV3Interface} from "foundry-chainlink-toolkit/src/interfaces/feeds/AggregatorV3Interface.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract TicketGo {
+    AggregatorV3Interface internal dataFeed;
+
+    /**
+     * Network: Sepolia
+     * Aggregator: ETH/USD
+     * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+     */
+    constructor() {
+        dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
     }
 
-    function increment() public {
-        number++;
+    // get leatest price ETH/USD
+    function getChainlinkDataFeedLatestAnswer() public view returns (int256) {
+        (
+            /* uint80 roundID */
+            ,
+            int256 answer,
+            /*uint startedAt*/
+            ,
+            /*uint timeStamp*/
+            ,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
     }
 }
