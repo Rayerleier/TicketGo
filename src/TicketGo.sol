203ee0d6dab85aa96079ca29d05b713c876347e5
Eric Dong <eric@Erics-MacBook-Air.local>
2024-05-25 17:14:38 +0800

    add booking selector with VRF

--- src/TicketGo.sol
+++ src/TicketGo.sol
@@ -1,10 +1,19 @@
 pragma solidity ^0.8.25;
 
 import {AggregatorV3Interface} from "@chainlink/contracts@1.1.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TicketGo is Ownable {
+contract TicketGo is Ownable VRFConsumerBaseV2{
     AggregatorV3Interface internal dataFeed;
+    
+    // Chainlink VRF Variables
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
 
     address public _operator;
     uint256 private concertId;
@@ -52,15 +61,20 @@ contract TicketGo is Ownable {
     event EvenetConcertCancelBought(uint256 indexed concertId,
         string indexed areaName,
         address audienceAddress);
+    event AreaBookingSelected(uint256 indexed concerId, string concertName, uint256[] selectedBookingAddress);
+    event ConcertSelected(uint256 indexed concerId);
+
     /**
      * Network: Sepolia
      * Aggregator: ETH/USD
      * Address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
      */
-    constructor() Ownable(msg.sender) {
+    // todo check how to contruct with multiple Inheritance
+    constructor(address vrfCoordinatorV2) Ownable(msg.sender) VRFConsumerBaseV2(vrfCoordinatorV2){
         dataFeed = AggregatorV3Interface(
             0x694AA1769357215DE4FAC081bf1f309aDC325306
         );
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     }
 
     function concertOf(uint256 _concertId)
@@ -215,6 +229,72 @@ contract TicketGo is Ownable {
         delete audiencePurchaseInfo[msg.sender][buyerinfoLength-1];
     }
 
+    // this function should be call by Automation.
+    // we can call it with a fixed Automation schedule, eg 00:00:00
+    // so we choice the final luck user at fix datetime.
+    // it's a tradeoff solution
+    function autoTrigger(uint8 concertId) external{
+        Concert concert = concertList[_concertId];
+        uint8 randomWordsCount = concert.Area.length()
+
+        s_raffleState = RaffleState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            randomWordsCount
+        );
+        emit RequestedConcertRandomWord(requestId);
+    }
+
+    /**
+     * @dev This is the function that Chainlink VRF node
+     * calls choice final booking.
+     */
+    function fulfillRandomWords(
+        uint256 /* requestId */,
+        uint256[] memory randomWords
+    ) internal override {
+        // s_players size 10
+        // randomNumber 202
+        // 202 % 10 ? what's doesn't divide evenly into 202?
+        // 20 * 10 = 200
+        // 2
+        // 202 % 10 = 2
+
+
+        mapping(concertID>mapping(string areaName>BuyerInfo[]) bookingMap
+        // {
+        //     "Area1": [{booking.userAddr}],
+        //     "Area2": [{booking.userAddr}],
+        //     "Area3": [{booking.userAddr}]
+
+        // }
+        for(uint i = 0; i < concert.Area.length; i++){
+            areaRandomWord = randomWords[i]
+            area = concert.Area[i]
+
+            areaBookings = bookingMap[area.areaName]
+            selectedAreaBookingIndexList = selectBookingForSingleArea(areaBookings, areaRandomWord)
+            emit AreaBookingSelected{}
+        }
+        emit ConcertSelected{}
+    } 
+
+    function selectForSingleArea(avaiableNum uint256, seed uint256) returns(uint256[] selectedIdxList){
+        uint256 seed = seed
+        uint256 a=1664525
+        uint256 c=1013904223
+        uint256 m=2**32
+        uint256[] memory selectedIdxList = new uint256[](avaiableNum)
+        while (i < avaiableNum){
+            idx = seed % avaiableNum
+            selectedIdxList[i] = idx
+            seed = (a * seed + c) % m
+        }
+    }
+    
     // get leatest price ETH/USD
     function getChainlinkDataFeedLatestAnswer() public view returns (int256) {
         (
